bc9242e7dd78cd779c5f1eef9c27537d8714c1cc
winsight <75029070+winsight@users.noreply.github.com>
2022-09-03 23:19:50 +0800

    2022-09-03

--- public/posts/write.sh
+++
@@ -1,6 +0,0 @@
-cd "Github repo"/Blog/zozo_hugo
-now=$(date "+%Y-%m-%d")
-hugo new posts/"$now.md"
-open /Users/wangshuai/GitHub\ repo/Blog/zozo_hugo/content/posts/$now.md
-cd content/posts
-mkdir "$now"
